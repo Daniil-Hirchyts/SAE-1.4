8eed5b4e670ea13127697288c5909bdd8b5cd3b5
Suuyano <starlordprod@gmail.com>
2022-12-16 19:24:31 +0100

    TODO R8, R4, R3

--- requetes.sql
+++ requetes.sql
@@ -1,26 +1,14 @@
 --R1 Combien de compétiteurs ont reçu au moins deux médailles d'or dans le stade avec la plus grande capacité d'accueil ?
-SELECT COUNT(num_competiteur) FROM(SELECT num_competiteur, COUNT(rang) AS nbRang
- FROM (SELECT num_stade, nbCap
-       FROM (SELECT num_competiteur
-             FROM (SELECT num_stade, nbCap
-                   FROM (SELECT num_stade, MAX(capacite) AS nbCap
-                         FROM Stades
+SELECT COUNT(num_competiteur) as nbCompetiteur FROM(SELECT num_competiteur, COUNT(rang) AS nbRang
+ FROM (SELECT *
+             FROM Epreuves e JOIN Recompense r ON e.num_Epreuve = r.num_Epreuve
+             WHERE num_stade = (SELECT num_stade FROM (SELECT num_stade, MAX(capacite) AS nbCap FROM Stades
                          GROUP by num_stade)
                    WHERE nbCap = (SELECT MAX(capacite) FROM Stades)
                    )
              )
-       ) -- MANQUE JOINTURE AVEC COMP
  WHERE rang ='Or'
  GROUP BY num_competiteur) WHERE nbRang >= 2
-
-SELECT COUNT(*)
-FROM (SELECT r.num_competiteur
-      FROM Recompense r
-               JOIN Epreuves e ON e.num_epreuve = r.num_epreuve
-               JOIN Stades s ON s.num_stade = e.num_stade
-               JOIN (SELECT num_stade, MAX(capacite) AS max_capacite FROM Stades GROUP BY num_stade) s2
-                    ON s2.num_stade = s.num_stade AND s.capacite = s2.max_capacite
-      )
 --R2 DONE
          SELECT nom, prenom, date_naissance
 FROM Competiteurs c
@@ -30,13 +18,22 @@ WHERE r.rang = 'Argent'
 AND c.nom_ville = c.pays_origine
 
 --R3
-
-
+Select num_competiteur
+FROM (Select num_competiteur, COUNT(r.num_epreuve) as nbEpreuve FROM (Select num_epreuve FROM Epreuves WHERE cat_e = 'Professionnel') e
+    JOIN Recompense r ON r.num_epreuve=e.num_epreuve
+    GROUP BY num_competiteur)
+WHERE nbEpreuve >= 2
 --R4
 
 
 --R5
-
+Select v.num_visiteur, v.nom, v.prenom, v.ville
+FROM Visiteurs v JOIN Place p ON v.num_visiteur = p.num_visiteur
+    JOIN stades s ON s.num_stade = p.num_stade
+    JOIN Epreuves e ON s.num_stade=e.num_stade
+    JOIN participe p1 ON p1.num_epreuve=e.num_epreuve
+    JOIN competiteurs c ON c.num_competiteur = p1.num_competiteur
+WHERE v.nom = c.nom AND v.ville = c.nom_ville
 
 --R6 DONE
 SELECT s.nom
@@ -45,13 +42,12 @@ FROM Epreuves e
 WHERE e.date_e = (SELECT MIN(date_e) FROM Epreuves)
 
 
---R7 FALSE
-SELECT c.num_competiteur, c.nom, c.prenom
-FROM Competiteurs c
-         JOIN Participe pa ON pa.num_competiteur = c.num_competiteur
-         JOIN Recompense r ON r.num_epreuve = pa.num_epreuve AND r.num_competiteur = c.num_competiteur
-         JOIN Epreuves e ON e.num_epreuve = pa.num_epreuve
-WHERE e.num_epreuve = (SELECT MAX(num_epreuve) FROM Epreuves)
+--R7 DONE
+Select c.nom, c.prenom
+FROM Recompense r
+    JOIN Epreuves e On e.num_epreuve = r.num_epreuve
+    JOIN competiteurs c ON c.num_competiteur = r.num_competiteur
+WHERE date_e = (select MAX(date_e) FROM epreuves)
 
 
 --R8 FALSE
