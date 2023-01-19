5c934a19f721153075da4e6c9a38e5155cde511c
Daniil Hirchyts <78032626+Daniil-Hirchyts@users.noreply.github.com>
2023-01-19 12:31:49 +0100

    Add files via upload

--- script.sql
+++ script.sql
@@ -1,18 +1,5 @@
 --||||||||||||||||||||||||||||||||||||||||||||--
--------------Suppression des tables-------------
---||||||||||||||||||||||||||||||||||||||||||||--
-
-DROP TABLE Participe;
-DROP TABLE Recompense;
-DROP TABLE Place;
-DROP TABLE Visiteurs;
-DROP TABLE Competiteurs;
-DROP TABLE Epreuves;
-DROP TABLE Stades;
-DROP TABLE Villes;
-
---||||||||||||||||||||||||||||||||||||||||||||--
---------------Creation des tables---------------
+----------------- Livrable 2 -------------------
 --||||||||||||||||||||||||||||||||||||||||||||--
 
 CREATE TABLE Villes
@@ -105,213 +92,59 @@ CREATE TABLE Participe
 );
 
 --||||||||||||||||||||||||||||||||||||||||||||--
---------------Insertion des tables--------------
+----------------- Livrable 3 -------------------
 --||||||||||||||||||||||||||||||||||||||||||||--
 
--- INSERT INTO Villes
-INSERT INTO Villes (nom_ville, num_depart, nb_habitant)
-VALUES ('Paris', 75, 1500);
-INSERT INTO Villes
-VALUES ('Londres', 702, 120);
-INSERT INTO Villes
-VALUES ('New York', 384, 4122);
-INSERT INTO Villes
-VALUES ('Tokyo', 659, 3423);
-INSERT INTO Villes
-VALUES ('Sydney', 304, 10);
-INSERT INTO Villes
-VALUES ('Rio de Janeiro', 213, 1234);
-INSERT INTO Villes
-VALUES ('Cape Town', 552, 12214);
-INSERT INTO Villes
-VALUES ('Moscou', 405, 1500);
-INSERT INTO Villes
-VALUES ('Pékin', 342, 3332);
-INSERT INTO Villes
-VALUES ('Mexico', 213, 2312);
 
+CREATE TABLE Date_a
+(
+    date_a DATE,
+    PRIMARY KEY (date_a)
+);
 
--- INSERT INTO Competiteurs
-INSERT INTO Competiteurs (num_competiteur, nom, prenom, date_inscription, date_naissance, pays_origine, nom_ville)
-VALUES (1, 'Djokovic', 'Novak', TO_DATE('2020-02-01', 'YYYY-MM-DD'), TO_DATE('1981-05-22', 'YYYY-MM-DD'), 'Serbie',
-        'Paris');
-INSERT INTO Competiteurs
-VALUES (2, 'Dupont', 'Lambargamber', TO_DATE('2020-01-01', 'YYYY-MM-DD'), TO_DATE('1987-05-22', 'YYYY-MM-DD'), 'Serbie',
-        'Paris');
-INSERT INTO Competiteurs
-VALUES (3, 'Federer', 'Roger', TO_DATE('2020-01-01', 'YYYY-MM-DD'), TO_DATE('1981-08-08', 'YYYY-MM-DD'), 'Suisse',
-        'New York');
-INSERT INTO Competiteurs
-VALUES (4, 'Zverev', 'Alexander', TO_DATE('2020-01-01', 'YYYY-MM-DD'), TO_DATE('1997-04-20', 'YYYY-MM-DD'), 'Allemagne',
-        'Tokyo');
-INSERT INTO Competiteurs
-VALUES (5, 'Thiem', 'Dominic', TO_DATE('2020-01-01', 'YYYY-MM-DD'), TO_DATE('1993-09-03', 'YYYY-MM-DD'), 'Autriche',
-        'Sydney');
-INSERT INTO Competiteurs
-VALUES (6, 'Osaka', 'Naomi', TO_DATE('2020-01-01', 'YYYY-MM-DD'), TO_DATE('1997-10-16', 'YYYY-MM-DD'), 'Japon',
-        'Rio de Janeiro');
-INSERT INTO Competiteurs
-VALUES (7, 'Kerber', 'Angelique', TO_DATE('2020-01-01', 'YYYY-MM-DD'), TO_DATE('1988-01-18', 'YYYY-MM-DD'), 'Allemagne',
-        'Cape Town');
-INSERT INTO Competiteurs
-VALUES (8, 'Serena', 'Venus', TO_DATE('2020-01-01', 'YYYY-MM-DD'), TO_DATE('1980-06-17', 'YYYY-MM-DD'), 'États-Unis',
-        'Moscou');
-INSERT INTO Competiteurs
-VALUES (9, 'Barty', 'Ashleigh', TO_DATE('2020-01-01', 'YYYY-MM-DD'), TO_DATE('1996-04-24', 'YYYY-MM-DD'), 'Australie',
-        'Pékin');
-INSERT INTO Competiteurs
-VALUES (10, 'Halep', 'Simona', TO_DATE('2020-01-01', 'YYYY-MM-DD'), TO_DATE('1991-09-27', 'YYYY-MM-DD'), 'Roumanie',
-        'Mexico');
+CREATE TABLE Date_p
+(
+    date_p DATE,
+    PRIMARY KEY (date_p)
+);
 
+ALTER TABLE Place
+    ADD CONSTRAINT fk_date_achat FOREIGN KEY (date_achat) REFERENCES Date_a (date_a);
+ALTER TABLE Participe
+    ADD date_p DATE;
+ALTER TABLE Participe
+    ADD CONSTRAINT fk_date_p_participe FOREIGN KEY (date_p) REFERENCES Date_p (date_p);
 
--- INSERT INTO Stades
-INSERT INTO Stades (num_stade, nom, adresse, capacite, ville)
-VALUES (1, 'Stade de France', 'Zac du Cornillon Nord, 93216 Saint-Denis', 80000, 'Paris');
-INSERT INTO Stades
-VALUES (2, 'Wembley Stadium', 'Wembley, London HA9 0WS, Royaume-Uni', 90000, 'Londres');
-INSERT INTO Stades
-VALUES (3, 'Newlands Stadium', 'Boundary Rd, Newlands, Le Cap, Afrique du Sud', 52000, 'Cape Town');
-INSERT INTO Stades
-VALUES (4, 'Tokyo Dome', '1-3-61 Koraku, Bunkyo City, Tokyo 112-0004, Japon', 55000, 'Tokyo');
-INSERT INTO Stades
-VALUES (5, 'ANZ Stadium', 'Olympic Park, Sydney NSW 2140, Australie', 83000, 'Sydney');
-INSERT INTO Stades
-VALUES (6, 'Maracanã', 'Rua Professor Eurico Rabelo, Maracanã, Rio de Janeiro, Brésil', 78000, 'Rio de Janeiro');
-INSERT INTO Stades
-VALUES (7, 'Newlands Stadium', 'Boundary Rd, Newlands, Le Cap, Afrique du Sud', 52000, 'Cape Town');
-INSERT INTO Stades
-VALUES (8, 'Luzhniki Stadium', 'Prospekt Vernadskogo, Moscou, Russie', 80000, 'Moscou');
-INSERT INTO Stades
-VALUES (9, 'Nest Stadium', 'Olympic Green, Chaoyang, Pékin, Chine', 90000, 'Pékin');
-INSERT INTO Stades
-VALUES (10, 'Estadio Azteca', 'Calle de Rio Churubusco s/n, Santa Ursula Coapa, Coyoacán, Mexico DF, Mexique', 95000,
-        'Mexico');
+INSERT INTO Date_p (date_p)
+VALUES ('01-01-2018');
+INSERT INTO Date_p (date_p)
+VALUES ('12-12-2020');
+INSERT INTO Date_p (date_p)
+VALUES ('14-02-2019');
+INSERT INTO Date_p (date_p)
+VALUES ('23-05-2018');
+INSERT INTO Date_p (date_p)
+VALUES ('11-04-2017');
+INSERT INTO Date_p (date_p)
+VALUES ('22-12-2020');
+INSERT INTO Date_p (date_p)
+VALUES ('07-08-2019');
+INSERT INTO Date_p (date_p)
+VALUES ('30-07-2018');
 
-
--- INSERT INTO Visiteurs
-INSERT INTO Visiteurs (num_visiteur, nom, prenom, ville)
-VALUES (1, 'Dupont', 'Jean', 'Paris');
-INSERT INTO Visiteurs
-VALUES (2, 'Durand', 'Marie', 'Paris');
-INSERT INTO Visiteurs
-VALUES (3, 'Martin', 'Luc', 'Londres');
-INSERT INTO Visiteurs
-VALUES (4, 'Petit', 'Jacques', 'Londres');
-INSERT INTO Visiteurs
-VALUES (5, 'Lefebvre', 'Sophie', 'New York');
-INSERT INTO Visiteurs
-VALUES (6, 'Garcia', 'Olivier', 'New York');
-INSERT INTO Visiteurs
-VALUES (7, 'Johnson', 'Emma', 'Tokyo');
-INSERT INTO Visiteurs
-VALUES (8, 'Smith', 'David', 'Tokyo');
-INSERT INTO Visiteurs
-VALUES (9, 'Williams', 'Samantha', 'Sydney');
-INSERT INTO Visiteurs
-VALUES (10, 'Jones', 'Michael', 'Sydney');
-
-
--- INSERT INTO Épreuves
-INSERT INTO Epreuves (num_epreuve, sport, cat_e, date_e, num_stade)
-VALUES (1, 'Tennis', 'Débutant', TO_DATE('2021-06-02 15:30:00', 'YYYY-MM-DD HH24:MI:SS'), 1);
-INSERT INTO Epreuves
-VALUES (2, 'Tennis', 'Débutant', TO_DATE('2015-08-04 10:10:00', 'YYYY-MM-DD HH24:MI:SS'), 1);
-INSERT INTO Epreuves
-VALUES (3, 'Football', 'Professionnel', TO_DATE('2018-04-10 21:20:00', 'YYYY-MM-DD HH24:MI:SS'), 2);
-INSERT INTO Epreuves
-VALUES (4, 'Football', 'Professionnel', TO_DATE('2020-09-18 22:40:00', 'YYYY-MM-DD HH24:MI:SS'), 2);
-INSERT INTO Epreuves
-VALUES (5, 'Natation', 'Intermédiaire', TO_DATE('2021-11-14 23:30:00', 'YYYY-MM-DD HH24:MI:SS'), 3);
-INSERT INTO Epreuves
-VALUES (6, 'Natation', 'Débutant', TO_DATE('2022-01-16 23:55:00', 'YYYY-MM-DD HH24:MI:SS'), 3);
-INSERT INTO Epreuves
-VALUES (7, 'Athlétisme', 'Intermédiaire', TO_DATE('2012-12-30 10:50:00', 'YYYY-MM-DD HH24:MI:SS'), 4);
-INSERT INTO Epreuves
-VALUES (8, 'Athlétisme', 'Professionnel', TO_DATE('2022-12-16 10:43:00', 'YYYY-MM-DD HH24:MI:SS'), 4);
-INSERT INTO Epreuves
-VALUES (9, 'Escrime', 'Intermédiaire', TO_DATE('2022-07-01 17:00:00', 'YYYY-MM-DD HH24:MI:SS'), 10);
-INSERT INTO Epreuves
-VALUES (10, 'Escrime', 'Professionnel', TO_DATE('2022-05-08 10:30:00', 'YYYY-MM-DD HH24:MI:SS'), 10);
-INSERT INTO Epreuves
-VALUES (11, 'Volley-ball', 'Intermédiaire', TO_DATE('2022-06-08 17:30:00', 'YYYY-MM-DD HH24:MI:SS'), 10);
-INSERT INTO Epreuves
-VALUES (12, 'Volley-ball', 'Professionnel', TO_DATE('2018-09-26 21:20:00', 'YYYY-MM-DD HH24:MI:SS'), 3);
-
-
--- INSERT INTO Place
-INSERT INTO Place (num_visiteur, num_stade, num_place, prix_place, date_achat)
-VALUES (1, 1, 1, 50.0, TO_DATE('2020-01-01', 'YYYY-MM-DD'));
-INSERT INTO Place
-VALUES (2, 1, 2, 50.0, TO_DATE('2020-01-01', 'YYYY-MM-DD'));
-INSERT INTO Place
-VALUES (3, 1, 3, 50.0, TO_DATE('2020-01-01', 'YYYY-MM-DD'));
-INSERT INTO Place
-VALUES (4, 1, 4, 50.0, TO_DATE('2020-01-01', 'YYYY-MM-DD'));
-INSERT INTO Place
-VALUES (5, 1, 5, 50.0, TO_DATE('2022-12-14', 'YYYY-MM-DD'));
-INSERT INTO Place
-VALUES (6, 1, 6, 50.0, TO_DATE('2020-01-01', 'YYYY-MM-DD'));
-INSERT INTO Place
-VALUES (7, 1, 7, 50.0, TO_DATE('2020-01-01', 'YYYY-MM-DD'));
-INSERT INTO Place
-VALUES (8, 1, 8, 50.0, TO_DATE('2020-01-01', 'YYYY-MM-DD'));
-INSERT INTO Place
-VALUES (9, 1, 9, 50.0, TO_DATE('2020-01-01', 'YYYY-MM-DD'));
-INSERT INTO Place
-VALUES (10, 1, 10, 50.0, TO_DATE('2020-01-01', 'YYYY-MM-DD'));
-INSERT INTO Place
-VALUES (2, 1, 4, 10.0, TO_DATE('2020-12-14', 'YYYY-MM-DD'));
-
-
--- INSERT INTO Participe
-INSERT INTO Participe (num_competiteur, num_epreuve)
-VALUES (1, 1);
-INSERT INTO Participe
-VALUES (2, 2);
-INSERT INTO Participe
-VALUES (3, 8);
-INSERT INTO Participe
-VALUES (2, 4);
-INSERT INTO Participe
-VALUES (5, 6);
-INSERT INTO Participe
-VALUES (6, 2);
-INSERT INTO Participe
-VALUES (7, 4);
-INSERT INTO Participe
-VALUES (8, 1);
-INSERT INTO Participe
-VALUES (9, 9);
-INSERT INTO Participe
-VALUES (10, 10);
-INSERT INTO Participe
-VALUES (10, 11);
-INSERT INTO Participe
-VALUES (3, 12);
-
-
--- INSERT INTO Recompense
-INSERT INTO Recompense (num_competiteur, num_epreuve, rang)
-VALUES (1, 1, 'Argent');
-INSERT INTO Recompense
-VALUES (2, 2, 'Or');
-INSERT INTO Recompense
-VALUES (3, 8, 'Bronze');
-INSERT INTO Recompense
-VALUES (10, 5, 'Or');
-INSERT INTO Recompense
-VALUES (5, 6, 'Argent');
-INSERT INTO Recompense
-VALUES (6, 2, 'Bronze');
-INSERT INTO Recompense
-VALUES (7, 4, 'Argent');
-INSERT INTO Recompense
-VALUES (8, 1, 'Argent');
-INSERT INTO Recompense
-VALUES (9, 9, 'Or');
-INSERT INTO Recompense
-VALUES (10, 10, 'Or');
-INSERT INTO Recompense
-VALUES (10, 11, 'Or');
-INSERT INTO Recompense
-VALUES (3, 12, 'Or');
+INSERT INTO Date_a
+VALUES ('21-03-2016');
+INSERT INTO Date_a
+VALUES ('18-11-2019');
+INSERT INTO Date_a
+VALUES ('07-04-2021');
+INSERT INTO Date_a
+VALUES ('23-05-2020');
+INSERT INTO Date_a
+VALUES ('07-01-2023');
+INSERT INTO Date_a
+VALUES ('08-10-2019');
+INSERT INTO Date_a
+VALUES ('11-03-2018');
+INSERT INTO Date_a
+VALUES ('14-10-2017');
